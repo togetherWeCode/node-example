418f60ba88eb20dba1ddfb8756d9387d28ddc61b
Richard-NG <richard.chihong.ng@gmail.com>
2018-01-05 00:52:13 +0800

    loggin some timestamps

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 require("express")()
     .get("/", (req, res, next) => {
+        var d = new Date()
+        console.log(`Request received at ${d.toUTCString()}`);
         res.send("Welcome To Node-Example II")
     })
     .listen(3000)
